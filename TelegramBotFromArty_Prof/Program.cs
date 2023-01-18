c3bde52d0ab45dab387316ecf4d4353625f036f6
Artur Khutak <arty4prof@gmail.com>
2023-01-18 22:14:37 +0200

    Convert to web app (#2)

--- TelegramBotFromArty_Prof/Program.cs
+++ TelegramBotFromArty_Prof/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Setup Services
+var app = builder.Build();
+
+// Map APIs
+app.MapGet("/", () => "Hello World!");
+
+// Start the Server
+app.Run();
