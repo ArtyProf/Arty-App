35b84d7f8a383e29640e49dafdc0898babb119bc
Artur Khutak <arty4prof@gmail.com>
2023-01-19 10:10:51 +0200

    Basic setup (#4)

---
+++ TelegramBotFromArty_Prof/Configuration/BotConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace TelegramBotfromArtyProf.Configuration;
+
+public class BotConfiguration
+{
+    public string BotToken { get; init; } = default!;
+    public string HostAddress { get; init; } = default!;
+    public string Route { get; init; } = default!;
+    public string SecretToken { get; init; } = default!;
+}
--- TelegramBotFromArty_Prof/Program.cs
+++ TelegramBotFromArty_Prof/Program.cs
@@ -1,10 +1,30 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Telegram.Bot;
+using TelegramBotfromArtyProf.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var botConfigurationSection = builder.Configuration.GetSection(nameof(BotConfiguration));
+builder.Services.Configure<BotConfiguration>(botConfigurationSection);
+
+var botConfiguration = botConfigurationSection.Get<BotConfiguration>() ?? throw new ArgumentNullException(nameof(BotConfiguration));
+
+
+builder.Services.AddHttpClient("telegram_bot_client")
+                .AddTypedClient<ITelegramBotClient>((httpClient) =>
+                {
+                    TelegramBotClientOptions options = new(botConfiguration.BotToken);
+                    return new TelegramBotClient(options, httpClient);
+                });
+
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 // Setup Services
 var app = builder.Build();
 
 // Map APIs
-app.MapGet("/", () => "Hello World! I am deployed!");
+app.MapControllers();
+app.MapGet("/", () => $"Hello World! I am deployed! See token: {botConfiguration.BotToken}");
 
 // Start the Server
 app.Run();
