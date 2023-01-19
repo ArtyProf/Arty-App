59805529afa0ae2a9ba2edecb20dbc3d1f4dbdc9
Artur Khutak <a.khutak@andersenlab.com>
2023-01-19 12:25:55 +0200

    Add webhook

---
+++ TelegramBotFromArty_Prof/Services/UpdateHandlers.cs
@@ -0,0 +1,133 @@
+﻿using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace TelegramBotfromArtyProf.Services;
+
+public class UpdateHandlers
+{
+    private readonly ITelegramBotClient _botClient;
+    private readonly ILogger<UpdateHandlers> _logger;
+
+    public UpdateHandlers(ITelegramBotClient botClient, ILogger<UpdateHandlers> logger)
+    {
+        _botClient = botClient;
+        _logger = logger;
+    }
+
+    public Task HandleErrorAsync(Exception exception, CancellationToken cancellationToken)
+    {
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        _logger.LogInformation("HandleError: {ErrorMessage}", ErrorMessage);
+        return Task.CompletedTask;
+    }
+
+    public async Task HandleUpdateAsync(Update update, CancellationToken cancellationToken)
+    {
+        var handler = update switch
+        {
+            { Message: { } message } => BotOnMessageReceived(message, cancellationToken),
+            { EditedMessage: { } message } => BotOnMessageReceived(message, cancellationToken),
+            { CallbackQuery: { } callbackQuery } => BotOnCallbackQueryReceived(callbackQuery, cancellationToken),
+            { InlineQuery: { } inlineQuery } => BotOnInlineQueryReceived(inlineQuery, cancellationToken),
+            { ChosenInlineResult: { } chosenInlineResult } => BotOnChosenInlineResultReceived(chosenInlineResult, cancellationToken),
+            _ => UnknownUpdateHandlerAsync(update, cancellationToken)
+        };
+
+        await handler;
+    }
+
+    private async Task BotOnMessageReceived(Message message, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Receive message type: {MessageType}", message.Type);
+        if (message.Text is not { } messageText)
+            return;
+
+        var action = messageText.Split(' ')[0] switch
+        {
+            "/start" => SendGreetings(_botClient, message, cancellationToken),
+            _ => Usage(_botClient, message, cancellationToken)
+        };
+        Message sentMessage = await action;
+        _logger.LogInformation("The message was sent with id: {SentMessageId}", sentMessage.MessageId);
+
+        static async Task<Message> SendGreetings(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+        {
+            return await botClient.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: "Greeting! You done first step!",
+                cancellationToken: cancellationToken);
+        }
+
+        static async Task<Message> Usage(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+        {
+            const string usage = "Usage:\n" +
+                                 "/start - Greeting";
+
+            return await botClient.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: usage,
+                replyMarkup: new ReplyKeyboardRemove(),
+                cancellationToken: cancellationToken);
+        }
+    }
+
+    // Process Inline Keyboard callback data
+    private async Task BotOnCallbackQueryReceived(CallbackQuery callbackQuery, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Received inline keyboard callback from: {CallbackQueryId}", callbackQuery.Id);
+
+        await _botClient.AnswerCallbackQueryAsync(
+            callbackQueryId: callbackQuery.Id,
+            text: $"Received {callbackQuery.Data}",
+            cancellationToken: cancellationToken);
+
+        await _botClient.SendTextMessageAsync(
+            chatId: callbackQuery.Message!.Chat.Id,
+            text: $"Received {callbackQuery.Data}",
+            cancellationToken: cancellationToken);
+    }
+
+    private async Task BotOnInlineQueryReceived(InlineQuery inlineQuery, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Received inline query from: {InlineQueryFromId}", inlineQuery.From.Id);
+
+        InlineQueryResult[] results = {
+            // displayed result
+            new InlineQueryResultArticle(
+                id: "1",
+                title: "TgBots",
+                inputMessageContent: new InputTextMessageContent("hello"))
+        };
+
+        await _botClient.AnswerInlineQueryAsync(
+            inlineQueryId: inlineQuery.Id,
+            results: results,
+            cacheTime: 0,
+            isPersonal: true,
+            cancellationToken: cancellationToken);
+    }
+
+    private async Task BotOnChosenInlineResultReceived(ChosenInlineResult chosenInlineResult, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Received inline result: {ChosenInlineResultId}", chosenInlineResult.ResultId);
+
+        await _botClient.SendTextMessageAsync(
+            chatId: chosenInlineResult.From.Id,
+            text: $"You chose result with Id: {chosenInlineResult.ResultId}",
+            cancellationToken: cancellationToken);
+    }
+
+    private Task UnknownUpdateHandlerAsync(Update update, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Unknown update type: {UpdateType}", update.Type);
+        return Task.CompletedTask;
+    }
+}
