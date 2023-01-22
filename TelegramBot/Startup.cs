a6983963601bb327ec022574d20ed119b89c41ce
Artur Khutak <arty4prof@gmail.com>
2023-01-22 14:45:47 +0200

    Add Sonarcloud check (#8)
    * add sonarcloud
    
    * add manual trigger for ci/cd

--- TelegramBot/Startup.cs
+++ TelegramBot/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Telegram.Bot;
 using TelegramBot.Configuration;
